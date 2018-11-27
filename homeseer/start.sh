f48e515f8723f683e43b50d77eb27d30d84ac3fd
berryk <berryk@vivopc.lan>
2018-11-26 21:36:47 -0500

    Updates

--- homeseer/start.sh
+++ homeseer/start.sh
@@ -3,5 +3,22 @@
 # bug fix for case sensitive filesystems
 # without this myhs.homeseer.com wont load icons
 #ln -s /HomeSeer/html/images/homeseer /HomeSeer/html/images/HomeSeer
+#
 
-mono /HomeSeer/HSConsole.exe --log
+if [ -e /DO_INSTALL ]
+then
+	echo "HomeSeer (re)install/update required at container first run. Installing at /HomeSeer..."
+
+        wget -O /homeseer.tgz "http://homeseer.com/updates3/hs3_linux_$HOMESEER_VERSION.tar.gz"
+	tar -xzo -C / -f /homeseer.tgz
+        rm -f /homeseer.tgz
+	rm -f /DO_INSTALL
+
+ else
+        echo "HomeSeer already installed, not (re)installing/updating..."
+
+fi
+
+cd /HomeSeer
+mono HSConsole.exe --log
+exit 0
